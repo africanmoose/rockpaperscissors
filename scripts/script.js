9a3da4e57199a7e24a51700c62369b6ece023ed7
africanmoose <mgathuru@gmail.com>
2023-08-02 13:23:25 +0300

    Add 5 series game code

--- scripts/script.js
+++ scripts/script.js
@@ -13,15 +13,91 @@ function getComputerChoice(){
     case 2:
         return "scissors"
         break;
-}
+    }
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+function game(){
+    //5 round game of rock paper scissors
+    //keep score and report winner at end
+
+    //variables
+    let intComputerScore = 0;
+    let intUserScore = 0;
+
+    //get user prompt
+    switch(playRound(prompt("Rock paper or scissors?"))){
+        case 0: //user wins
+            intUserScore = intUserScore + 1;
+            break;
+        case 1: //computer wins
+            intComputerScore = intComputerScore + 1;
+            break;
+        case 3: 
+            //draw: do nothing
+    }
+
+        switch(playRound(prompt("Rock paper or scissors?"))){
+        case 0: //user wins
+            intUserScore = intUserScore + 1;
+            break;
+        case 1: //computer wins
+            intComputerScore = intComputerScore + 1;
+            break;
+        case 3: 
+            //draw: do nothing
+    }
+
+        switch(playRound(prompt("Rock paper or scissors?"))){
+        case 0: //user wins
+            intUserScore = intUserScore + 1;
+            break;
+        case 1: //computer wins
+            intComputerScore = intComputerScore + 1;
+            break;
+        case 3: 
+            //draw: do nothing
+    }
+
+        switch(playRound(prompt("Rock paper or scissors?"))){
+        case 0: //user wins
+            intUserScore = intUserScore + 1;
+            break;
+        case 1: //computer wins
+            intComputerScore = intComputerScore + 1;
+            break;
+        case 3: 
+            //draw: do nothing
+    }
+
+        switch(playRound(prompt("Rock paper or scissors?"))){
+        case 0: //user wins
+            intUserScore = intUserScore + 1;
+            break;
+        case 1: //computer wins
+            intComputerScore = intComputerScore + 1;
+            break;
+        case 3: 
+            //draw: do nothing
+    }
+
+    if (intUserScore > intComputerScore){
+        console.log ("You win! You scored: " + intUserScore + " to Computer: " + intComputerScore)
+        } 
+        else if (intUserScore < intComputerScore){
+        console.log ("You Lose! You scored: " + intUserScore + " to Computer: " + intComputerScore)
+        }
+        else {
+        console.log ("You drew. You scored: " + intUserScore + " to Computer: " + intComputerScore)
+        }
+    
+}
+
 function playRound(getUserChoice){
-    //case insensitivie
+    //case insensitive
     let strUserChoice = getUserChoice.toLowerCase();
 
     // variables
@@ -31,7 +107,9 @@ function playRound(getUserChoice){
     //compare results, return winner
     //first eliminate draw situation
     if (strComputerChoice == strUserChoice)
-         {intComputerWin = 2}
+        {
+            intComputerWin = 2
+        }
     else {
         switch(strUserChoice){
         case "rock":
@@ -48,19 +126,26 @@ function playRound(getUserChoice){
             if (strComputerChoice == "scissors") {
                 intComputerWin = 1;
                 }
-                break;
+            break;
         }
-    }
-
-    switch (intComputerWin) {
-        case 0: //user wins
-            console.log("you win!");
-            break;
-        case 1: //user loses
-            console.log("you lose!");
-            break;
-        case 2: //draw
-            console.log("you draw, try again");
-    }
+ 
+        }
+    
+    //output to console
+    getPrettyMessage(intComputerWin, strComputerChoice, strUserChoice);
+    return intComputerWin;
 
 }
+
+function getPrettyMessage(intMessageNumber, strComputerChoice, strUserChoice){
+    switch (intMessageNumber) {
+        case 0: //user wins
+            console.log("you win! " + strUserChoice + " beats " + strComputerChoice);
+            break;
+        case 1: //user loses
+            console.log("you lose! " + strComputerChoice + " beats " + strUserChoice);
+            break;
+        case 2: //draw
+            console.log("you draw, try again. You both chose " + strComputerChoice);
+        }
+}   
